7d126ab27af55241600a7f59d50f4b19fd6cf99d
Arpad Borsos <swatinem@swatinem.de>
2025-06-11 14:02:08 +0200

    draft a core driver

--- stresstest/src/driver.rs
+++ stresstest/src/driver.rs
@@ -1,26 +1,66 @@
-use tokio::sync::Semaphore;
+use std::sync::Arc;
 
-use crate::generator::BatchGenerator;
+use tokio::sync::Semaphore;
+use tokio::task::JoinSet;
+
+use crate::generator::{BatchGenerator, GeneratorAction};
 use crate::workload::Payload;
 
-pub trait RemoteFs {
-    type ExternalId;
-    async fn write(payload: Payload) -> Self::ExternalId;
-    async fn read(id: Self::ExternalId, payload: Payload);
-    async fn delete(id: Self::ExternalId);
+pub trait RemoteFs: Send + Sync {
+    type ExternalId: Send + 'static;
+    async fn write(&self, payload: Payload) -> Self::ExternalId;
+    async fn read(&self, id: Self::ExternalId, payload: Payload);
+    async fn delete(&self, id: Self::ExternalId);
 }
 
 pub struct Driver<Fs: RemoteFs> {
     concurrency: Semaphore,
-    remote: Fs,
+    remote: Arc<Fs>,
     generator: BatchGenerator<Fs::ExternalId>,
 }
 
-impl<Fs: RemoteFs> Driver<Fs>
+impl<Fs: RemoteFs + Send + 'static> Driver<Fs>
 where
     Fs::ExternalId: Clone,
 {
     pub async fn run_stresstest(&mut self) {
-        let batch = self.generator.generate_actions_batch(100);
+        let mut batch = self.generator.generate_actions_batch(100);
+
+        let semaphore = Arc::new(Semaphore::new(20));
+        let mut joinset = JoinSet::new();
+        loop {
+            let permit = semaphore.clone().acquire_owned().await.unwrap();
+
+            let Some(action) = batch.pop() else {
+                let new_ids = joinset.join_all().await;
+                for new_id in new_ids {
+                    if let Some((internal_id, external_id)) = new_id {
+                        self.generator.register_id(internal_id, external_id);
+                    }
+                }
+                break;
+            };
+
+            let remote = Arc::clone(&self.remote);
+            let task = async move {
+                let new_id = match action {
+                    GeneratorAction::Write(internal_id, payload) => {
+                        let external_id = remote.write(payload).await;
+                        Some((internal_id, external_id))
+                    }
+                    GeneratorAction::Read(external_id, payload) => {
+                        remote.read(external_id, payload).await;
+                        None
+                    }
+                    GeneratorAction::Delete(external_id) => {
+                        remote.delete(external_id).await;
+                        None
+                    }
+                };
+                drop(permit);
+                new_id
+            };
+            joinset.spawn(task);
+        }
     }
 }
